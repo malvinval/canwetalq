9d58ddf98c536eb75d0f438204ef20d5ce3df016
Malvin Valerian <malvinval09@gmail.com>
2023-06-24 18:07:04 +0700

    update: navbar

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('chat')} active={route().current('chat')}>
+                                    Private Chat
+                                </NavLink>
                             </div>
                         </div>
 
@@ -94,6 +97,9 @@ export default function Authenticated({ auth, header, children }) {
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('chat')} active={route().current('chat')}>
+                            Private Chat
                         </ResponsiveNavLink>
                     </div>
 
